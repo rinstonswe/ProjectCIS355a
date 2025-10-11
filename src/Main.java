4ff1d321279a9a9490bf41e8b5387dee379172f0
rinstonSWE <geoffrey.baker421@gmail.com>
2025-10-11 14:27:29 -0500

    Week 4

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,52 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main
 {
     // class level references and variables
     private static Scanner scan = new Scanner(System.in);
-    protected static DecimalFormat fmt = new DecimalFormat("0.00");
-    private static double total = 0.0;
-    private static String order = "";
+    public static DecimalFormat fmt = new DecimalFormat("#,##0.00");
+    private static ArrayList<Stock> stockList = new ArrayList<Stock>();
 
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
         int choice;
-        final int EXIT_VALUE = 4;
+        final int EXIT_VALUE = 5;
 
         do {
             //show menu
             System.out.println("Menu\n");
-            System.out.println("1) Add to Order");
-            System.out.println("2) Show Current Order");
-            System.out.println("3) New Order");
-            System.out.println("4) Exit");
+            System.out.println("1) Add Stock");
+            System.out.println("2) Remove Stock");
+            System.out.println("3) Display Single Stock");
+            System.out.println("4) Show Stock Portfolio");
+            System.out.println("5) Exit");
 
             //get users choice
             System.out.println("\nEnter your choice: ");
             choice = scan.nextInt();
 
-            switch (choice){
+            switch (choice) {
                 case 1:
-                    addToOrder();
-                    showCurrentOrder();
                     scan.nextLine();
-                    System.out.println("Press ENTER to continue...\n\n");
+                    System.out.println("Case 1");
                     scan.nextLine();
                     break;
                 case 2:
-                    showCurrentOrder();
                     scan.nextLine();
-                    System.out.println("Press ENTER to continue...\n\n");
+                    System.out.println("Case 2");
                     scan.nextLine();
                     break;
                 case 3:
-                    total = 0;
-                    order = "";
-                    showCurrentOrder();
                     scan.nextLine();
-                    System.out.println("Press ENTER to continue...\n\n");
+                    System.out.println("Case 3");
                     scan.nextLine();
                     break;
                 case 4:
+                    scan.nextLine();
+                    System.out.println("Case 4");
+                    scan.nextLine();
+                case 5:
                     System.out.println("Goodbye!");
                     break;
                 default:        //Normally for error checking
@@ -58,45 +55,5 @@ public class Main
             }
 
         } while (choice != EXIT_VALUE);
-    }//ends main method
-    public static void addToOrder()
-    {
-        Burger burger = new Burger();
-        String option;
-        System.out.print("Single or Double burger (single/double)? ");
-        option = scan.next();
-        if (option.equalsIgnoreCase("Single"))
-        {burger.setType("Single");}
-        else if  (option.equalsIgnoreCase("Double"))
-        {burger.setType("Double");}
-        else {System.out.println("\nInvalid burger type");return;}
-        System.out.println("Item Price: " + fmt.format(burger.calculateItemCost()));
-
-        System.out.print("Add cheese (y/n)? ");
-        option = scan.next();
-        burger.setCheese(option.equalsIgnoreCase("y"));
-        System.out.println("Item Price: " + fmt.format(burger.calculateItemCost()));
-
-        System.out.print("Add bacon (y/n)? ");
-        option = scan.next();
-        burger.setBacon(option.equalsIgnoreCase("y"));
-        System.out.println("Item Price: " + fmt.format(burger.calculateItemCost()));
-
-        System.out.print("Make it a Meal (y/n)? ");
-        option = scan.next();
-        burger.setMeal(option.equalsIgnoreCase("y"));
-        System.out.println("Item Price: " + fmt.format(burger.calculateItemCost()) + "\n");
-
-        System.out.println("How many of these burgers to add to the order (0 to cancel)? ");
-        int quantity = scan.nextInt();
-        burger.setQuantity(quantity);
-
-        order = order + burger.toString();
-        total = burger.calculateTotalCost();
     }
-
-    public static void showCurrentOrder() {
-        System.out.println("Current Order\n" + order + "\nOrder Total: " + fmt.format(total) + "\n");
-    }
-
-}//Ends Main class
+}
---
+++ src/Stock.java
@@ -0,0 +1,11 @@
+public class Stock
+{
+    // create your attributes here
+
+    // create your constructors here
+
+    // create your behaviors here
+
+    // create your getters and setters here
+
+}
